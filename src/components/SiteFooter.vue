0e73110df068c1c67a981a14e4ffef28274004c8
sangb2014876 <sangb2014876@student.ctu.edu.vn>
2023-11-20 18:32:17 +0700

    frontend

---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,10 @@
+<template>
+    <footer class="bg-gray-200 text-center lg:text-left">
+        <div class="text-gray-700 text-center p-4"
+        style="background-color: rgba(0, 0, 0, 0.2)">
+2023 coppyright:
+<a class="text-gray-800" href="https://abc.com">abc.com</a>
+        </div>
+
+    </footer>
+</template>
