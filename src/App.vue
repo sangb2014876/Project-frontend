f835338af8b5c8fff5744a2bb9ee6ea577395b1d
sangb2014876 <sangb2014876@student.ctu.edu.vn>
2023-11-20 18:39:46 +0700

    frontend

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterView } from "vue-router"
+import SiteNavbar from "./components/SiteNavbar.vue";
+import SiteFooter from "./components/SiteFooter.vue";
+</script>
+
+<template>
+  <div class="col-20">
+    <div class=" flex flex-col h-screen">
+    <SiteNavbar />
+     <main class="flex-grow">
+      <RouterView />
+     </main>
+     <SiteFooter />
+  </div>
+  </div>
+  
+  
+</template>
